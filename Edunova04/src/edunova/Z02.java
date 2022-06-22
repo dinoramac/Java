7233ab32ea595183a2de6c2eec109e6f7261a0c1
Dino Ramač <64275745+dinoramac@users.noreply.github.com>
2022-06-22 15:39:32 +0200

    popravak
    popravljeno, pravilni ispis greske i zaustavljanje programa

--- Edunova04/src/edunova/Z02.java
+++ Edunova04/src/edunova/Z02.java
@@ -19,19 +19,20 @@ public class Z02 {
 					tablica[i][j] = (i + 1) * (j + 1);
 				}
 			}
+			for (int i = 0; i < unos; i++) {
+				for (int j = 0; j < unos; j++) {
+					if (tablica[i][j] < 10) {
+						System.out.print("  ");
+					} else if (tablica[i][j]>=10 && tablica[i][j]<100) {
+						System.out.print(" ");
+					}
+					System.out.print(tablica[i][j] + " ");
+				}
+				System.out.print("\n");
+			}
 		}
 
-		for (int i = 0; i < unos; i++) {
-			for (int j = 0; j < unos; j++) {
-				if (tablica[i][j] < 10) {
-					System.out.print("  ");
-				} else if (tablica[i][j]>=10 && tablica[i][j]<100) {
-					System.out.print(" ");
-				}
-				System.out.print(tablica[i][j] + " ");
-			}
-			System.out.print("\n");
-		}
+		
 
 	}
 }
