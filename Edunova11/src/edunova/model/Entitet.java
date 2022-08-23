7f81e9b15e6e817f9d0ca49fe630fc6f99471309
Dino Ramač <64275745+dinoramac@users.noreply.github.com>
2022-08-23 22:58:52 +0200

    11. vjezba edunova

---
+++ Edunova11/src/edunova/model/Entitet.java
@@ -0,0 +1,14 @@
+package edunova.model;
+
+public abstract class Entitet {
+
+	private String sifra;
+
+	public String getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(String sifra) {
+		this.sifra = sifra;
+	}
+}
